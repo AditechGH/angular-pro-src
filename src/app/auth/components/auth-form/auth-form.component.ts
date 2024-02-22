5b45cd2b58fbbd269d55ca29b05830157d3484b9
Adinan Alhassan <aditech5170@gmail.com>
2024-02-22 19:55:02 +0000

    @ContentChild and ngAfterContentInit.

--- src/app/auth/components/auth-form/auth-form.component.ts
+++ src/app/auth/components/auth-form/auth-form.component.ts
@@ -1,25 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 
 import { User } from '../../model/user.model';
 
 @Component({
   selector: 'auth-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   template: `
     <div class="auth-form">
-    <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
+      <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
         <ng-content select="h3"></ng-content>
         <label>
           Email address
-          <input type="email" name="email" ngModel>
+          <input type="email" name="email" ngModel />
         </label>
         <label>
           Password
-          <input type="password" name="password" ngModel>
+          <input type="password" name="password" ngModel />
         </label>
         <ng-content select="auth-remember"></ng-content>
+        <div *ngIf="showMessage">You will be logged in for 30 days</div>
         <ng-content select="button"></ng-content>
       </form>
     </div>
@@ -45,15 +55,27 @@ import { User } from '../../model/user.model';
             border: 1px solid #ccc;
           }
           .email {
-            border-color: #9f72e6
+            border-color: #9f72e6;
           }
         }
       }
     `,
   ],
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements AfterContentInit {
+  showMessage: boolean = false;
+
+  @ContentChild('remember') remember!: AuthRememberComponent;
+
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+  ngAfterContentInit(): void {
+    if (this.remember) {
+      this.remember.checked.subscribe(
+        (checked: boolean) => (this.showMessage = checked)
+      );
+    }
+  }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
