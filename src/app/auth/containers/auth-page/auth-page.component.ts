27b20ee830b4d320e78f03e439d37f85e9b1c1c0
Adinan Alhassan <aditech5170@gmail.com>
2024-02-23 08:23:42 +0000

    Dynamic <ng-template> rendering with View.

--- src/app/auth/containers/auth-page/auth-page.component.ts
+++ src/app/auth/containers/auth-page/auth-page.component.ts
@@ -1,13 +1,12 @@
 import {
+  AfterContentInit,
   Component,
   ComponentRef,
   OnInit,
+  TemplateRef,
+  ViewChild,
   ViewContainerRef,
 } from '@angular/core';
-
-import { AuthFormComponent } from '../../components/auth-form/auth-form.component';
-
-import { User } from '../../model/user.model';
 
 @Component({
   selector: 'auth-page',
@@ -15,42 +14,27 @@ import { User } from '../../model/user.model';
   imports: [],
   template: `
     <div class="auth-page">
-      <button (click)="destroyComponent()">Destroy</button>
-      <button (click)="moveComponent()">Move</button>
       <div #entry></div>
+      <ng-template #tmpl> Adnan Alhassan : Ghana, West Africa </ng-template>
     </div>
   `,
-  styles: [`
-    .auth-page {
-      button {
-        margin: 0 0 20px 30px;
+  styles: [
+    `
+      .auth-page {
+        button {
+          margin: 0 0 20px 30px;
+        }
       }
-    }
-  `],
+    `,
+  ],
 })
-export class AuthPageComponent implements OnInit {
-  component!: ComponentRef<AuthFormComponent>;
+export class AuthPageComponent implements AfterContentInit {
+  @ViewChild('tmpl', { read: TemplateRef, static: true })
+  tmpl!: TemplateRef<any>;
 
   constructor(private viewContainerRef: ViewContainerRef) {}
 
-  ngOnInit(): void {
-    this.viewContainerRef.createComponent(AuthFormComponent);
-    this.component = this.viewContainerRef.createComponent(AuthFormComponent, {
-      index: 0,
-    });
-    this.component.instance.title = 'Create account';
-    this.component.instance.submitted.subscribe(this.loginUser);
-  }
-
-  destroyComponent() {
-    this.component.destroy();
-  }
-
-  moveComponent() {
-    this.viewContainerRef.move(this.component.hostView, 1);
-  }
-
-  loginUser(user: User) {
-    console.log('Login', user);
+  ngAfterContentInit(): void {
+    this.viewContainerRef.createEmbeddedView(this.tmpl);
   }
 }
