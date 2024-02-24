e0a689c88299cf0808d0435d585ab6418019992f
Adinan Alhassan <aditech5170@gmail.com>
2024-02-24 11:43:55 +0000

    Componentizing FormGroups.

---
+++ src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'stock-products',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <div class="stock-product" [formGroup]="parent">
+
+    </div>
+  `,
+  styleUrl: './stock-products.component.scss'
+})
+export class StockProductsComponent {
+  @Input() parent!: FormGroup;
+}
