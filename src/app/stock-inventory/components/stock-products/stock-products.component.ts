c2e2a224ee7a2a674d310a80f94c02fe42207444
Adinan Alhassan <aditech5170@gmail.com>
2024-02-24 18:13:18 +0000

    Organize imports.

--- src/app/stock-inventory/components/stock-products/stock-products.component.ts
+++ src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, NgForOf } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
   AbstractControl,
   FormArray,
