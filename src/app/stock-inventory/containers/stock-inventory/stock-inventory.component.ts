e7023ace1d1d3e395ee15c8825c34529ef0c3666
Adinan Alhassan <aditech5170@gmail.com>
2024-02-24 16:26:01 +0000

    FormBuilder API.

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -2,7 +2,7 @@ import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormArray,
-  FormControl,
+  FormBuilder,
   FormGroup,
   ReactiveFormsModule,
 } from '@angular/forms';
@@ -59,22 +59,24 @@ export class StockInventoryComponent {
     { id: 5, price: 600, name: 'Apple Watch' },
   ];
 
-  form = new FormGroup({
-    store: new FormGroup({
-      branch: new FormControl(''),
-      code: new FormControl(''),
+  form = this.fb.group({
+    store: this.fb.group({
+      branch: '',
+      code: '',
     }),
     selector: this.createStock({}),
-    stock: new FormArray([
+    stock: this.fb.array([
       this.createStock({ product_id: 1, quantity: 10 }),
       this.createStock({ product_id: 3, quantity: 50 }),
     ]),
   });
 
+  constructor(private fb: FormBuilder) {}
+
   createStock(stock: Stock) {
-    return new FormGroup({
-      product_id: new FormControl(stock.product_id || 0),
-      quantity: new FormControl(stock.quantity || 10),
+    return this.fb.group({
+      product_id: stock.product_id || 0,
+      quantity: stock.quantity || 10,
     });
   }
 
