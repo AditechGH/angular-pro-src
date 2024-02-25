46c1009f1fdbcc1793e5dbe659577e686fd8d43e
Adinan Alhassan <aditech5170@gmail.com>
2024-02-25 21:33:21 +0000

    Preloading lazy-loaded modules.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withDebugTracing } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes /* withDebugTracing() */)],
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules))],
 };
