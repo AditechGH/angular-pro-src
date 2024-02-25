ca080a8b684aa7f4ae29a113c289f7bf84a5092c
Adinan Alhassan <aditech5170@gmail.com>
2024-02-25 18:56:34 +0000

    Auxiliary Navigation API.

--- src/app/mail/components/mail-item/mail-item.component.ts
+++ src/app/mail/components/mail-item/mail-item.component.ts
@@ -2,18 +2,14 @@ import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'mail-item',
   standalone: true,
   imports: [RouterModule, DatePipe],
   template: `
-    <a
-      class="mail-item"
-      [routerLink]="['', { outlets: { pane: ['message', message.id] } }]"
-      routerLinkActive="active"
-    >
+    <a class="mail-item" (click)="navigateToMessage()">
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date : 'shortTime' }}</span>
@@ -25,4 +21,13 @@ import { RouterModule } from '@angular/router';
 })
 export class MailItemComponent {
   @Input() message!: Mail;
+
+  constructor(private router: Router) {}
+
+  navigateToMessage() {
+    this.router.navigate([
+      '',
+      { outlets: { pane: ['message', this.message?.id] } },
+    ]);
+  }
 }
