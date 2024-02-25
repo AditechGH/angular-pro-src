9274dd74b6e4f8c6a34653efb96fd757dce6857e
Adinan Alhassan <aditech5170@gmail.com>
2024-02-25 12:10:46 +0000

    Subscribing to router events.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,5 @@ import { provideRouter, withDebugTracing } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withDebugTracing())],
+  providers: [provideRouter(routes /* withDebugTracing() */)],
 };
