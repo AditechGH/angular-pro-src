c121cbf62c45088b29345d05a565ad003eb7e4ce
Adinan Alhassan <aditech5170@gmail.com>
2024-02-28 17:23:00 +0000

    Using InjectionToken.

--- src/app/viewer/food.service.ts
+++ src/app/viewer/food.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { API_TOKEN } from '../token';
 
 @Injectable()
 export class FoodService {
-  constructor(private http: HttpClient, @Inject('api') private api: string) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(API_TOKEN) private api: string
+  ) {}
 
   getFood(): Observable<any> {
-    console.log(this.api);
     return this.http.get(this.api);
   }
 }
