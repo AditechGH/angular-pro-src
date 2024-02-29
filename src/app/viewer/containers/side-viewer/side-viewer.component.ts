e15f2dd294847931b272780cf7d2eafc3f9fffd5
Adinan Alhassan <aditech5170@gmail.com>
2024-02-29 06:21:45 +0000

    Providers and useFactory.

--- src/app/viewer/containers/side-viewer/side-viewer.component.ts
+++ src/app/viewer/containers/side-viewer/side-viewer.component.ts
@@ -1,19 +1,31 @@
 import { AsyncPipe, CurrencyPipe, NgForOf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { FoodService } from '../../food.service';
+
 
 interface Side {
   name: string;
   price: number;
 }
 
+export function SideFactory(http: HttpClient) {
+  return new FoodService(http, '/api/sides');
+}
+
 @Component({
   selector: 'side-viewer',
   standalone: true,
   imports: [NgForOf, AsyncPipe, CurrencyPipe],
-  providers: [FoodService],
+  providers: [
+    {
+      provide: FoodService,
+      useFactory: SideFactory,
+      deps: [HttpClient],
+    },
+  ],
   template: `
     <div>
       <div *ngFor="let item of items$ | async">
--- src/app/viewer/food.service.ts
+++ src/app/viewer/food.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { API_TOKEN } from '../token';
 
 @Injectable()
 export class FoodService {
   constructor(
     private http: HttpClient,
-    @Inject(API_TOKEN) private api: string
-  ) {}
+    private api: String
+  ) {
+    console.log(this.api)
+  }
 
   getFood(): Observable<any> {
-    return this.http.get(this.api);
+    return this.http.get(this.api as string);
   }
 }
