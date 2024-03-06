2208c33f431ca9f44c99e60bd3efdf952b5708d7
Adinan Alhassan <aditech5170@gmail.com>
2024-03-06 21:53:00 +0000

    Destroying dynamic components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  AfterContentInit,
+  Component,
+  ComponentRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
-import { AuthPageComponent } from './auth/containers/auth-page/auth-page.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+import { User } from './auth-form/auth-form.interface';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthPageComponent],
-  template: `<auth-page></auth-page>`,
-  styles: [``],
+  imports: [],
+  template: `
+    <div class="auth-page">
+      <button (click)="destroyComponent()">Destroy</button>
+      <div #entry></div>
+    </div>
+  `,
 })
-export class AppComponent {}
+export class AppComponent implements AfterContentInit {
+  component!: ComponentRef<AuthFormComponent>;
+
+  @ViewChild('entry', { read: ViewContainerRef, static: true })
+  entry!: ViewContainerRef;
+
+  ngAfterContentInit(): void {
+    // const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+    // ComponentFactoryResolver is deprecated in Angular versions 13 and above
+    // Using the createComponent method of ViewContainerRef to create the component instance and attach it directly to the entry element.
+    this.component = this.entry.createComponent(AuthFormComponent);
+    this.component.instance.title = 'Create account';
+    this.component.instance.submitted.subscribe(this.loginUser);
+  }
+
+  destroyComponent() {
+    this.component.destroy();
+  }
+
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
+}
