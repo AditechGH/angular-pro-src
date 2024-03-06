fcf04c7f52218ac97e53e42f1930b1e527014e7e
Adinan Alhassan <aditech5170@gmail.com>
2024-03-06 19:08:19 +0000

    Refactor code - Projecting and binding to components.

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,15 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { User } from '../../model/user.model';
+import { User } from './auth-form.interface';
 
 @Component({
   selector: 'auth-form',
   standalone: true,
   imports: [FormsModule],
   template: `
-    <div class="auth-form">
-    <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
+    <div>
+      <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
         <ng-content select="h3"></ng-content>
         <label>
           Email address
@@ -24,33 +24,6 @@ import { User } from '../../model/user.model';
       </form>
     </div>
   `,
-  styles: [
-    `
-      .auth-form {
-        &:first-child {
-          form {
-            border-right: 1px solid rgba(0, 0, 0, 0.1);
-          }
-        }
-        form {
-          flex: 1 0;
-          padding: 0 40px;
-          input {
-            display: block;
-            border: none;
-            background: #ffffff;
-            font-size: 13px;
-            padding: 6px 30px 6px 10px;
-            border-radius: 1px;
-            border: 1px solid #ccc;
-          }
-          .email {
-            border-color: #9f72e6
-          }
-        }
-      }
-    `,
-  ],
 })
 export class AuthFormComponent {
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
