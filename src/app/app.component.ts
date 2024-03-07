3759ab971db4d051abe536d22b8f3ef0b4fa6867
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 07:40:19 +0000

    Creating a custom pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
-import { FilesComponent } from './files/containers/files/files.component';
+import { FilesizePipe } from './filesize.pipe';
+
+interface File {
+  name: string,
+  size: number,
+  type: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilesComponent],
-  template: `<files></files>`,
-  styles: [``],
+  imports: [NgForOf, FilesizePipe],
+  template: `
+    <div>
+      <div *ngFor="let file of files">
+        <p>{{ file.name }}</p>
+        <p>{{ file.size | filesize }}</p>
+      </div>
+    </div>
+  `
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  files!: File[];
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' }
+    ];
+  }
+}
