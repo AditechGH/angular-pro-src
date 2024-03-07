04db114c0c3253b21f58191edb9829bfc290a937
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 07:47:14 +0000

    Pipes as providers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
-import { FilesComponent } from './files/containers/files/files.component';
+import { FileSizePipe } from './filesize.pipe';
+
+interface File {
+  name: string;
+  size: any;
+  type: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilesComponent],
-  template: `<files></files>`,
-  styles: [``],
+  imports: [NgForOf],
+  providers: [FileSizePipe],
+  template: `
+    <div>
+      <div *ngFor="let file of mapped">
+        <p>{{ file.name }}</p>
+        <p>{{ file.size }}</p>
+      </div>
+    </div>
+  `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  files!: File[];
+  mapped!: File[];
+
+  constructor(private fileSizePipe: FileSizePipe) {}
+
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' },
+    ];
+    this.mapped = this.files.map((file) => {
+      return {
+        name: file.name,
+        type: file.type,
+        size: this.fileSizePipe.transform(file.size, 'mb'),
+      };
+    });
+  }
+}
