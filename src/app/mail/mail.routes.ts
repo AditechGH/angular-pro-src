7f9f40c8d2e611838fba47184a316bca1a50bde8
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 14:21:02 +0000

    Enabling route tracing.

--- src/app/mail/mail.routes.ts
+++ src/app/mail/mail.routes.ts
@@ -1,9 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { Routes } from '@angular/router';
 
 import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
 
-// routes
-export const routes: Routes = [
-  { path: ':name', component: MailFolderComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'inbox' },
+export const mailRoutes: Routes = [
+  {
+    path: 'folder/:name',
+    component: MailFolderComponent,
+    providers: [importProvidersFrom(HttpClientModule)],
+  },
 ];
