e77c25496e5772bc8d12d8165d31f83bde3963b7
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 18:59:30 +0000

    Preloading lazy-loaded modules.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
   withPreloading,
 } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+import { MailService } from './mail/mail.service';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules))],
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(HttpClientModule),
+    MailService,
+  ],
 };
