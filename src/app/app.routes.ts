59a0dfed0cbab77cdd85a231bcb86ea9a98c5435
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 18:47:07 +0000

    Lazy-loading modules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { Routes } from '@angular/router';
-import { MailService } from './mail/mail.service';
+
+import { mailRoutes } from './mail/mail.routes';
 
 export const routes: Routes = [
-  {
-    path: 'mail',
-    loadChildren: () => import('./mail/mail.routes').then((x) => x.routes),
-    providers: [importProvidersFrom(HttpClientModule), MailService],
-  },
+  ...mailRoutes,
   {
     path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.routes').then((x) => x.routes)
+    loadChildren: () =>
+      import('./dashboard/dashboard.routes').then((x) => x.routes),
   },
-  {
-    path: '**',
-    redirectTo: 'mail/folder/inbox',
-  },
+  { path: '**', redirectTo: 'mail/folder/inbox' },
 ];
--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
