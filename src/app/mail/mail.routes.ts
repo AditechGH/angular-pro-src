714d51c6d35a18d04d2b016edba0f33b95a812c7
Adinan Alhassan <aditech5170@gmail.com>
2024-03-07 17:25:45 +0000

    Auxiliary named router outlets.

--- src/app/mail/mail.routes.ts
+++ src/app/mail/mail.routes.ts
@@ -1,23 +1,21 @@
 import { Routes } from '@angular/router';
 
+import { MailViewComponent } from './components/mail-view/mail-view.component';
 import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
 
 import { mailFolderResolver } from './containers/mail-folder/mail-folder.resolve';
-import { MailViewComponent } from './components/mail-view/mail-view.component';
 
-// routes
-export const routes: Routes = [
+export const mailRoutes: Routes = [
   {
     path: 'folder/:name',
     component: MailFolderComponent,
     resolve: {
-      messages: mailFolderResolver
-    }
+      messages: mailFolderResolver,
+    },
   },
   {
     path: 'message/:id',
     component: MailViewComponent,
     outlet: 'pane'
-  },
-  { path: '', pathMatch: 'full', redirectTo: 'folder/inbox' },
+  }
 ];
