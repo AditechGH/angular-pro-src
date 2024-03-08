fc043008dfa7bedbd494f551e3fc8067c6739c92
Adinan Alhassan <aditech5170@gmail.com>
2024-03-08 06:38:19 +0000

    Testing isolate Pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { MailAppComponent } from './mail/components/mail-app/mail-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, MailAppComponent],
-  template: `
-    <div class="app">
-      <header>
-        <img src="assets/img/logo.svg" />
-      </header>
-      <div class="app__content">
-        <nav>
-          <a
-            [routerLink]="[
-              '/mail',
-              { outlets: { primary: 'folder/inbox', pane: null } }
-            ]"
-            routerLinkActive="active"
-          >
-            Inbox
-          </a>
-          <a
-            [routerLink]="[
-              '/mail',
-              { outlets: { primary: 'folder/trash', pane: null } }
-            ]"
-            routerLinkActive="active"
-          >
-            Trash
-          </a>
-          <a [routerLink]="['/dashboard']" routerLinkActive="active">
-            Dashboard
-          </a>
-        </nav>
-        <router-outlet></router-outlet>
-      </div>
-    </div>
-  `,
-  styleUrl: 'app.component.scss',
+  imports: [],
+  template: ``,
 })
 export class AppComponent {}
--- src/app/filesize.pipe.spec.ts
+++ src/app/filesize.pipe.spec.ts
@@ -1,20 +1,24 @@
 import { FilesizePipe } from './filesize.pipe';
 
 describe('FileSizePipe', () => {
+
   describe('Isolate FileSizePipe test', () => {
+
     const pipe = new FilesizePipe();
 
     it('should convert bytes to megabytes', () => {
       expect(pipe.transform(123456789)).toBe('117.74MB');
       expect(pipe.transform(987654321)).toBe('941.90MB');
     });
+
     it('should use the default extension when not supplied', () => {
       expect(pipe.transform(123456789)).toBe('117.74MB');
       expect(pipe.transform(987654321)).toBe('941.90MB');
     });
+
     it('should override the extension when supplied', () => {
       expect(pipe.transform(123456789, 'myExt')).toBe('117.74myExt');
-      expect(pipe.transform(987654321, 'myExt')).toBe('941.90myExt');
+      expect(pipe.transform(987654321, 'anotherExt')).toBe('941.90anotherExt');
     });
   });
 });
